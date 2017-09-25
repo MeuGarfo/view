0b9d869426f1117c182f4b8031ffc102b304589f
Anderson Ismael <aicoutos@users.noreply.github.com>
2017-09-25 13:49:27 -0300

    A  src/View.php

---
+++ src/View.php
@@ -0,0 +1,28 @@
+<?php
+namespace Basic;
+class View{
+    public $view;
+    function setView($name,$data=null,$print=true){
+        if($name=='404'){
+            header('HTTP/1.0 404 Not Found');
+        }
+        $filename=ROOT.'view/'.$name.'.php';
+        $data['data']=$data;
+        $data['viewName']=$name;
+        if(file_exists($filename)){
+            extract($data);
+            ob_start();
+            require_once($filename);
+            $output = ob_get_contents();
+            ob_end_clean();
+            if($print){
+                print $output;
+            }else{
+                return $output;
+            }
+        }else{
+            die('<h1>Error</h1>View <b>'.$filename.'</b> not found');
+        }
+    }
+}
+?>
